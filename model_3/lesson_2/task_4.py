0622cb7388f8c9484cb15dc74554088b57171a87
user <yunusboyevfarrux@gmail.com>
2023-01-27 15:40:36 +0500

    .

--- model_3/lesson_2/task_4.py
+++ model_3/lesson_2/task_4.py
@@ -9,6 +9,8 @@ def only_even_parameters(func):
                 return 'Please add only even numbers!'
 
     return divide_inner
+
+
 @only_even_parameters
 def add(a, b):
     return a + b
@@ -16,9 +18,12 @@ def add(a, b):
 
 print(add(6, 8))  # 14
 print(add(1, 4))  # Please add only even numbers!
+
+
 @only_even_parameters
 def multiply(a, b, c, d):
     return a * b * c * d
 
-print(multiply(2,3,4,2))
-print(multiply(3,3,4,2))
+
+print(multiply(2, 3, 4, 2))
+print(multiply(3, 3, 4, 2))
