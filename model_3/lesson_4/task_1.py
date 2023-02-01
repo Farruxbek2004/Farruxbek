bea3a1a551efdfdd1b9b6c2f4e116952b682d5ee
user <yunusboyevfarrux@gmail.com>
2023-02-01 23:40:27 +0500

    .

--- model_3/lesson_4/task_1.py
+++ model_3/lesson_4/task_1.py
@@ -6,28 +6,29 @@ from tkinter.messagebox import showerror
 age = Tk()
 age.title('P10-Age Calculator')
 age.resizable(width=False, height=False)
+age.configure(background="green")
 name = Frame(age, height=400, width=400, bg='white', )
 name.grid(row=0, column=1)
 age_name = Label(age, text="Age calculator", foreground="red", font=30)
 age_name.place(x=150, y=10)
-label_data = Label(age, text="Day: ", bg='cyan', font=25)
+label_data = Label(age, text="Day: ", bg='green', font=25)
 label_data.place(x=70, y=40)
 day_entry = Entry(age, width=5, font=(None, 10, 'bold'))
 day_entry.place(x=150, y=40)
 
-label_month = Label(age, text="Month: ", bg='cyan', font=25)
+label_month = Label(age, text="Month: ", bg='green', font=25)
 label_month.place(x=70, y=80)
 month_entry = Entry(age, width=5)
 month_entry.place(x=150, y=80)
 
-label_year = Label(age, text="Year: ", bg='cyan', font=25)
+label_year = Label(age, text="Year: ", bg='green', font=25)
 label_year.place(x=70, y=120)
 year_entry = Entry(age, width=5)
 year_entry.place(x=150, y=120)
 calculator_age0 = Label(age, text="Calculate age ->", foreground="green", font=25)
 calculator_age0.place(x=20, y=170)
 
-label_name = Label(age, text="Your age: ", bg='yellow', font=50)
+label_name = Label(age, text="Your age: ", bg='green', font=50)
 label_name.place(x=50, y=250)
 
 
