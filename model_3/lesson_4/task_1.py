8e11c860489c5751238e2e962604a70b78b945d9
user <yunusboyevfarrux@gmail.com>
2023-02-01 14:47:58 +0500

    .

--- model_3/lesson_4/task_1.py
+++ model_3/lesson_4/task_1.py
@@ -3,20 +3,28 @@
 
 
 from tkinter import *
+from datetime import date
+from tkinter import Entry, Label, Button
 
 window = Tk()
 window.title("Age calculator")
 window.geometry("300x400")
+
+
+def age():
+    value = date.today()
+    year = int(year_entry.get())
+    if year <= 0 or year >= 2023:
+        year_label['text'] == "Xato"
+    else:
+        info = value.year - year
+        Label['text'] == str(info)
+
+
 year_label = Label(window, text="Birth year:")
 year_label.place(x=0, y=15)
 year_entry = Entry(window, width=20, borderwidth=3)
 year_entry.place(x=60, y=15)
-
-
-def func():
-    my_button.config(text="your name: 23")
-
-
-my_button = Button(window, text="OK", command=func)
-my_button.place(x=55, y=50)
+year_button = Button(text="Age", command=age)
+year_button.place(x=100, y=35)
 window.mainloop()
