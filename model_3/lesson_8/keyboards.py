913aa7cebc2afcd25e8d8ebaa2e572e24a56cb8f
user <yunusboyevfarrux@gmail.com>
2023-02-15 13:21:14 +0500

    .

--- model_3/lesson_8/keyboards.py
+++ model_3/lesson_8/keyboards.py
@@ -7,7 +7,7 @@ from telebot.types import (
 
 languages_btn = ReplyKeyboardMarkup(resize_keyboard=True)
 result_btn = ReplyKeyboardMarkup(resize_keyboard=True)
-registratsion_btn = ReplyKeyboardMarkup(resize_keyboard=True)
+
 LANGUAGES = {
     "UZ 🇺🇿": "uz",
     "RU 🇷🇺": "ru",
@@ -20,49 +20,30 @@ languages_btn.add(
     KeyboardButton(list(LANGUAGES.keys())[2])
 )
 
-languages_inline_btn = InlineKeyboardMarkup()
 
-languages_inline_btn.add(
-    InlineKeyboardButton(list(LANGUAGES.keys())[0], callback_data=f"language_{list(LANGUAGES.values())[0]}"),
-    InlineKeyboardButton(list(LANGUAGES.keys())[1], callback_data=f"language_{list(LANGUAGES.values())[1]}"),
-    InlineKeyboardButton(list(LANGUAGES.keys())[2], callback_data=f"language_{list(LANGUAGES.values())[2]}"),
-)
+def get_language_btn(value):
+    languages_inline_btn = InlineKeyboardMarkup()
+    languages_inline_btn.add(
+        InlineKeyboardButton(list(LANGUAGES.keys())[0],
+                             callback_data=f"{value}_language_{list(LANGUAGES.values())[0]}"),
+        InlineKeyboardButton(list(LANGUAGES.keys())[1],
+                             callback_data=f"{value}_language_{list(LANGUAGES.values())[1]}"),
+        InlineKeyboardButton(list(LANGUAGES.keys())[2], callback_data=f"{value}_language_{list(LANGUAGES.values())[2]}")
+    )
+    return languages_inline_btn
+
 
 share_phone_btn = ReplyKeyboardMarkup(resize_keyboard=True)
 share_phone_btn.add(KeyboardButton("Share phone", request_contact=True))
-
-
 RESULT = {
-    "❌": "no",
-    "✅": "yes"
+    "❌" : "no",
+    "✅" : "yes"
 }
 
-result_btn.add(
-    KeyboardButton(list(RESULT.keys())[0]),
-    KeyboardButton(list(RESULT.keys())[1])
-)
-
-result_inline_btn = InlineKeyboardMarkup()
-result_inline_btn.add(
-    InlineKeyboardButton(list(RESULT.keys())[0], callback_data=f'result_{list(RESULT.values())[0]}'),
-    InlineKeyboardButton(list(RESULT.keys())[1], callback_data=f'result_{list(RESULT.values())[1]}')
-)
-REGISTRATSION = {
-    "UZ 🇺🇿": "uz",
-    "RU 🇷🇺": "ru",
-    "EN 🇬🇧": "en"
-}
-
-registratsion_btn.add(
-    KeyboardButton(list(REGISTRATSION.keys())[0]),
-    KeyboardButton(list(REGISTRATSION.keys())[1]),
-    KeyboardButton(list(REGISTRATSION.keys())[2])
-)
-languages_registratsion_btn = InlineKeyboardMarkup()
-
-languages_registratsion_btn.add(
-    InlineKeyboardButton(list(REGISTRATSION.keys())[0], callback_data=f"registratsion_{list(REGISTRATSION.values())[0]}"),
-    InlineKeyboardButton(list(REGISTRATSION.keys())[1], callback_data=f"registratsion_{list(REGISTRATSION.values())[1]}"),
-    InlineKeyboardButton(list(REGISTRATSION.keys())[2], callback_data=f"registratsion_{list(REGISTRATSION.values())[2]}")
-)
+def get_result_btn():
+    result_inline_btn = InlineKeyboardMarkup()
+    result_inline_btn.add(
+        InlineKeyboardButton(list(RESULT.keys())[0], callback_data=f"result_{list(RESULT.keys())[0]}"),
+        InlineKeyboardButton(list(RESULT.keys())[1], callback_data=f"result_{list(RESULT.keys())[1]}"),
+    )
 
--- model_3/lesson_8/todo/main.py
+++ model_3/lesson_8/todo/main.py
@@ -4,12 +4,11 @@ import csv
 from telebot import custom_filters
 from telebot.storage import StateMemoryStorage
 from telebot.types import BotCommand, ReplyKeyboardRemove
-from datetime import datetime
-from model_3.lesson_8.keyboards import languages_inline_btn, share_phone_btn, result_inline_btn, languages_registratsion_btn
-from model_3.lesson_8.todo.messages import messages, result
+from model_3.lesson_8.keyboards import share_phone_btn, get_language_btn, get_result_btn
+from model_3.lesson_8.todo.messages import messages
 from model_3.lesson_8.todo.states import StudentRegistrationForm
-from model_3.lesson_8.todo.task import Chat, Task
-from model_3.lesson_8.utils import write_row_to_csv, get_fullname, get_language_code_by_chat_id
+from model_3.lesson_8.todo.task import Chat
+from model_3.lesson_8.utils import write_row_to_csv, get_fullname
 
 BOT_TOKEN = "6093522078:AAF1M1C1a-cCMHWnOPnan5B4B0NEmQ6PCnI"
 
@@ -24,10 +23,10 @@ def welcome_message(message):
     chat_id = message.chat.id
     user = message.from_user
     fullname = get_fullname(user.first_name, user.last_name)
-    bot.send_message(chat_id, f"Assalomu alaykum, {fullname}", reply_markup=languages_inline_btn)
+    bot.send_message(chat_id, f"Assalomu alaykum, {fullname}", reply_markup=get_language_btn("register"))
 
 
-@bot.callback_query_handler(lambda call: call.data.startswith("language_"))
+@bot.callback_query_handler(lambda call: call.data.startswith("_language_"))
 def set_language_query_handler(call):
     message = call.message
     lang_code = call.data.split("_")[1]
@@ -79,32 +78,28 @@ def phone_get(message):
 
 @bot.message_handler(state=StudentRegistrationForm.age)
 def age_get(message):
-    bot.send_message(message.chat.id, 'Tilni tanlang:', reply_markup=languages_registratsion_btn)
+    bot.send_message(message.chat.id, 'Tilni tanlang:', reply_markup=get_language_btn("course"))
     bot.set_state(message.from_user.id, StudentRegistrationForm.language, message.chat.id)
     with bot.retrieve_data(message.from_user.id, message.chat.id) as data:
         data['age'] = message.text
 
 
-@bot.callback_query_handler(lambda call: call.data.startswith("registratsion_"))
-def language_query_handler(call):
+@bot.callback_query_handler(lambda call: call.data.startswith("course_language_"),
+                            state=StudentRegistrationForm.language)
+def language_get(call):
     message = call.message
-    lang_code = call.data.split("_")[1]
-    chat = message.chat
-    # bot.delete_message(chat.id, message.id)
-    bot.send_message(chat.id, result[lang_code].get('res'))
-
-
-@bot.message_handler(state=StudentRegistrationForm.language)
-def language_get(message):
+    lang_code = call.data.split("_")[2]
     bot.send_message(message.chat.id, 'Kursni kiriting:')
     bot.set_state(message.from_user.id, StudentRegistrationForm.course, message.chat.id)
-    print(message.text)
     with bot.retrieve_data(message.from_user.id, message.chat.id) as data:
-        data['language'] = message.text
+        data['language'] = lang_code
+        print(lang_code)
 
 
-@bot.message_handler(state=StudentRegistrationForm.course)
-def course_get(message):
+@bot.callback_query_handler(lambda call: call.data.startswith("confirm_course_"), state=StudentRegistrationForm.course)
+def get_task_handler(call):
+    message = call.message
+    confirm_answer = call.data.split("_")[2]
     with bot.retrieve_data(message.from_user.id, message.chat.id) as data:
         data['course'] = message.text
         msg = "Quyidagi ma'lumotlar qa'bul qilindi:\n"
@@ -116,71 +111,25 @@ def course_get(message):
         msg += "ushbu malumotlar saqlansinmi ?\n"
         msg += "ha -> ✅\n"
         msg += "yo'q -> ❌"
-        bot.send_message(message.chat.id, msg, parse_mode="html", reply_markup=result_inline_btn)
-        with open("registratsion.csv", 'a', newline="\n") as f:
-            header = ['first_name', 'last_name', "phone", "age", "language", "course"]
-            first_name = data.get('first_name')
-            last_name = data.get('last_name')
-            phone = data.get('phone')
-            age = data.get('age')
-            language = data.get('language')
-            course = data.get('course')
-            dict_writer = csv.writer(f)
-            if os.path.getsize('registratsion.csv') == 0:
-                dict_writer.writerow(header)
-            dict_writer.writerow([first_name, last_name, phone, age, language, course])
-    # bot.delete_state(message.from_user.id, message.chat.id)
-
-
-@bot.callback_query_handler(lambda call: call.data.startswith("result_"))
-def set_result(call):
-    message = call.message
-    lang_code = call.data.split('_')[1]
-    print(lang_code)
-    chat = message.chat
-    bot.delete_message(chat.id, message.id)
-    bot.send_message(message.chat.id, value[lang_code].get("res"))
-
-
-value = {
-    'no': {
-        "res": "/register"
-    },
-    "yes": {
-        "res": "Sizning ma'lumotlaringgiz csv file ga saqlandi"
-    }
-}
-
-
-# /add
-@bot.message_handler(commands=["add"])
-def add_task_handler(message):
-    chat_id = message.chat.id
-    lang_code = get_language_code_by_chat_id(chat_id, "chats.csv")
-    msg = messages[lang_code].get("send_task")
-    bot.send_message(message.chat.id, msg)
-
-
-@bot.message_handler(content_types=["text"])
-def get_task_handler(message):
-    chat_id = message.chat.id
-    if message.content_type != "text":
-        bot.send_message(chat_id, "Invalid format.")
-
-    new_task = Task(chat_id, message.text, datetime.now())
-    write_row_to_csv(
-        "tasks.csv",
-        list(new_task.get_attrs_as_dict().keys()),
-        new_task.get_attrs_as_dict()
-    )
-
-    bot.send_message(chat_id, "Add successfully.")
+        bot.send_message(message.chat.id, msg, parse_mode="html", reply_markup=get_result_btn())
+        if confirm_answer == "yes":
+            with open("registratsion.csv", "a", encoding="utf-8") as f:
+                header = ["Fullname", "Phone", "Age", "Language", "Course"]
+                csv_write = csv.DictWriter(f, header)
+            if os.path.getsize("registratsion.csv") == 0:
+                csv_write.writeheader()
+            csv_write.writerow(message.from_user)
+            bot.send_message(message.chat.id, "Malumotlar csv file ga qo'shild")
+        elif confirm_answer == "no":
+            bot.send_message(message.chat.id, "Malumotlar saqlanmadi qaytadan /register buyrug'ini yuboring")
+        bot.send_message(message.chat.id, msg, "Ushbu ma'lumotlar saqlansinmi")
+        bot.delete_state(message.from_user.id, message.chat.id)
 
 
 def my_commands():
     return [
         BotCommand("/start", "Start bot"),
-        BotCommand("/add", "Add new task"),
+        # BotCommand("/add", "Add new task"),
         BotCommand("/register", "Register student")
     ]
 
